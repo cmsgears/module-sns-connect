899d5a6294f19ba3fce0bd0d9d71ad923e64dab2
Bhagwat Chouhan <bhagwat.chouhan@vulpinecode.com>
2015-11-26 00:30:08 +0530

    Updated menu view.

--- widgets/views/simple.php
+++ widgets/views/simple.php
@@ -3,7 +3,7 @@
 use cmsgears\social\login\common\config\SnsLoginGlobal;
 ?>
 
-<div class="row clearfix">
+<div class="row menu-social clearfix">
 <?php
 	foreach ( $settings as $key => $setting ) {
 		
@@ -14,21 +14,21 @@ use cmsgears\social\login\common\config\SnsLoginGlobal;
 				case SnsLoginGlobal::CONFIG_SNS_FACEBOOK: {
 	
 ?>
-	<div class="col12x4"> <a class="btn social facebook" href="<?= $setting->getLoginUrl() ?>"> <i class="cmti cmti-social-facebook"> </i> <em class="margin-left-5">FACEBOOK</em> </a></div>
+	<div class="col12x4"> <a class="btn facebook" href="<?= $setting->getLoginUrl() ?>"> <i class="cmti cmti-social-facebook"> </i> <em class="margin-left-5">FACEBOOK</em> </a></div>
 <?php
 					break;
 				}
 				case SnsLoginGlobal::CONFIG_SNS_GPLUS: {
 
 ?>
-	<div class="col12x4"> <a class="btn social google" href="<?= $setting->getLoginUrl() ?>"> <i class="cmti cmti-social-google"> </i> <em class="margin-left-5">GOOGLE</em> </a></div>
+	<div class="col12x4"> <a class="btn google" href="<?= $setting->getLoginUrl() ?>"> <i class="cmti cmti-social-google"> </i> <em class="margin-left-5">GOOGLE</em> </a></div>
 <?php	
 					break;
 				}
 				case SnsLoginGlobal::CONFIG_SNS_TWITTER: {
 
 ?>
-	<div class="col12x4"> <a class="btn social twitter" href="<?= $setting->getLoginUrl() ?>"> <i class="cmti cmti-social-twitter"> </i> <em class="margin-left-5">TWITTER </em></a></div>
+	<div class="col12x4"> <a class="btn twitter" href="<?= $setting->getLoginUrl() ?>"> <i class="cmti cmti-social-twitter"> </i> <em class="margin-left-5">TWITTER </em></a></div>
 <?php	
 					break;
 				}
