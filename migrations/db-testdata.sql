31b7cd0430353f790f5e1b44c277ddfade329e12
Bhagwat Chouhan <bhagwat.chouhan@vulpinecode.com>
2015-12-31 20:12:15 +0530

    Started new admin.

--- migrations/db-testdata.sql
+++ migrations/db-testdata.sql
@@ -8,12 +8,12 @@ SELECT @site := `id` FROM cmg_core_site WHERE slug = 'main';
 -- Facebook Config Form
 --
 
-INSERT INTO `cmg_core_form` (`siteId`,`templateId`,`createdBy`,`modifiedBy`,`name`,`slug`,`type`,`description`,`successMessage`,`captcha`,`visibility`,`active`,`userMail`,`adminMail`,`options`,`createdAt`,`modifiedAt`) VALUES
+INSERT INTO `cmg_core_form` (`siteId`,`templateId`,`createdBy`,`modifiedBy`,`name`,`slug`,`type`,`description`,`successMessage`,`captcha`,`visibility`,`active`,`userMail`,`adminMail`,`htmlOptions`,`createdAt`,`modifiedAt`) VALUES
 	(@site,NULL,1,1,'Config Facebook','config-facebook','system','Facebook configuration form.','All configurations saved successfully.',0,10,1,0,0,NULL,'2014-10-11 14:22:54','2014-10-11 14:22:54');
 
 SELECT @form := `id` FROM cmg_core_form WHERE slug = 'config-facebook';
 
-INSERT INTO `cmg_core_form_field` (`formId`,`name`,`label`,`type`,`compress`,`validators`,`options`,`data`,`order`) VALUES 
+INSERT INTO `cmg_core_form_field` (`formId`,`name`,`label`,`type`,`compress`,`validators`,`htmlOptions`,`data`,`order`) VALUES 
 	(@form,'active','Active',20,0,'required','{\"title\":\"Check whether Facebook Login is active.\"}',NULL,0),
 	(@form,'app_id','Application Id',0,0,'required','{\"title\":\"Application Id.\",\"placeholder\":\"Application Id\"}',NULL,0),
 	(@form,'app_secret','Application Secret',5,0,'required','{\"title\":\"Application Secret.\",\"placeholder\":\"Application Secret\"}',NULL,0),
@@ -23,12 +23,12 @@ INSERT INTO `cmg_core_form_field` (`formId`,`name`,`label`,`type`,`compress`,`va
 -- Google Config Form
 --
 
-INSERT INTO `cmg_core_form` (`siteId`,`templateId`,`createdBy`,`modifiedBy`,`name`,`slug`,`type`,`description`,`successMessage`,`captcha`,`visibility`,`active`,`userMail`,`adminMail`,`options`,`createdAt`,`modifiedAt`) VALUES
+INSERT INTO `cmg_core_form` (`siteId`,`templateId`,`createdBy`,`modifiedBy`,`name`,`slug`,`type`,`description`,`successMessage`,`captcha`,`visibility`,`active`,`userMail`,`adminMail`,`htmlOptions`,`createdAt`,`modifiedAt`) VALUES
 	(@site,NULL,1,1,'Config Google','config-google','system','Google configuration form.','All configurations saved successfully.',0,10,1,0,0,NULL,'2014-10-11 14:22:54','2014-10-11 14:22:54');
 
 SELECT @form := `id` FROM cmg_core_form WHERE slug = 'config-google';
 
-INSERT INTO `cmg_core_form_field` (`formId`,`name`,`label`,`type`,`compress`,`validators`,`options`,`data`,`order`) VALUES 
+INSERT INTO `cmg_core_form_field` (`formId`,`name`,`label`,`type`,`compress`,`validators`,`htmlOptions`,`data`,`order`) VALUES 
 	(@form,'active','Active',20,0,'required','{\"title\":\"Check whether Google Login is active.\"}',NULL,0),
 	(@form,'app_id','Application Id',0,0,'required','{\"title\":\"Application Id.\",\"placeholder\":\"Application Id\"}',NULL,0),
 	(@form,'app_secret','Application Secret',5,0,'required','{\"title\":\"Application Secret.\",\"placeholder\":\"Application Secret\"}',NULL,0),
@@ -38,12 +38,12 @@ INSERT INTO `cmg_core_form_field` (`formId`,`name`,`label`,`type`,`compress`,`va
 -- Twitter Config Form
 --
 
-INSERT INTO `cmg_core_form` (`siteId`,`templateId`,`createdBy`,`modifiedBy`,`name`,`slug`,`type`,`description`,`successMessage`,`captcha`,`visibility`,`active`,`userMail`,`adminMail`,`options`,`createdAt`,`modifiedAt`) VALUES
+INSERT INTO `cmg_core_form` (`siteId`,`templateId`,`createdBy`,`modifiedBy`,`name`,`slug`,`type`,`description`,`successMessage`,`captcha`,`visibility`,`active`,`userMail`,`adminMail`,`htmlOptions`,`createdAt`,`modifiedAt`) VALUES
 	(@site,NULL,1,1,'Config Twitter','config-twitter','system','Twitter site configuration form.','All configurations saved successfully.',0,10,1,0,0,NULL,'2014-10-11 14:22:54','2014-10-11 14:22:54');
 
 SELECT @form := `id` FROM cmg_core_form WHERE slug = 'config-twitter';
 
-INSERT INTO `cmg_core_form_field` (`formId`,`name`,`label`,`type`,`compress`,`validators`,`options`,`data`,`order`) VALUES 
+INSERT INTO `cmg_core_form_field` (`formId`,`name`,`label`,`type`,`compress`,`validators`,`htmlOptions`,`data`,`order`) VALUES 
 	(@form,'active','Active',20,0,'required','{\"title\":\"Check whether Google Login is active.\"}',NULL,0),
 	(@form,'api_key','API Id',0,0,'required','{\"title\":\"API Key.\",\"placeholder\":\"API Key\"}',NULL,0),
 	(@form,'api_secret','API Secret',5,0,'required','{\"title\":\"API Secret.\",\"placeholder\":\"API Secret\"}',NULL,0),
@@ -53,16 +53,16 @@ INSERT INTO `cmg_core_form_field` (`formId`,`name`,`label`,`type`,`compress`,`va
 -- Dumping data for table `cmg_core_model_attribute`
 --
 
-INSERT INTO `cmg_core_model_attribute` (`parentId`,`parentType`,`name`,`value`,`type`) VALUES
-	(@site,'site','active','1','facebook'),
-	(@site,'site','app_id','','facebook'),
-	(@site,'site','app_secret','','facebook'),
-	(@site,'site','redirect_uri','/sns/facebook/authorise','facebook'),
-	(@site,'site','active','1','google'),
-	(@site,'site','app_id','','google'),
-	(@site,'site','app_secret','','google'),
-	(@site,'site','redirect_uri','/sns/google/authorise','google'),
-	(@site,'site','active','1','twitter'),
-	(@site,'site','api_key','','twitter'),
-	(@site,'site','api_secret','','twitter'),
-	(@site,'site','redirect_uri','/sns/twitter/authorise','twitter');
+INSERT INTO `cmg_core_model_attribute` (`parentId`,`parentType`,`name`,`type`,`valueType`,`value`) VALUES
+	(@site,'site','active','facebook','flag','1'),
+	(@site,'site','app_id','facebook','text',''),
+	(@site,'site','app_secret','facebook','text',''),
+	(@site,'site','redirect_uri','facebook','text','/sns/facebook/authorise'),
+	(@site,'site','active','google','flag','1'),
+	(@site,'site','app_id','google','text',''),
+	(@site,'site','app_secret','google','text',''),
+	(@site,'site','redirect_uri','google','text','/sns/google/authorise'),
+	(@site,'site','active','twitter','flag','1'),
+	(@site,'site','api_key','twitter','text',''),
+	(@site,'site','api_secret','twitter','text',''),
+	(@site,'site','redirect_uri','twitter','text','/sns/twitter/authorise');
